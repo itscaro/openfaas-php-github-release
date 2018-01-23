6bd49fed83e5ed9637a5eea5e2b09e8a13b12e04
Minh-Quan TRAN <account@itscaro.me>
2018-01-23 15:40:52 +0100

    function to get latest release on GitHub
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

---
+++ README.md
@@ -0,0 +1,36 @@
+Function to get the latest release on GitHub and URL to the asset
+
+Request
+
+```json
+{
+    "repo": "openfaas/faas-cli"
+}
+```
+
+Response
+
+```json
+{
+    "version": "1.2.3"
+}
+```
+
+Request
+
+```json
+{
+    "repo": "openfaas/faas-cli",
+    "arch": "linux"
+}
+```
+
+Response
+
+```json
+{
+    "version": "1.2.3",
+    "name":"faas-cli",
+    "url":"https://github.com/openfaas/faas-cli/releases/download/0.5.2/faas-cli"
+}
+```
--- src/Handler.php
+++ src/Handler.php
@@ -8,39 +8,65 @@ use GuzzleHttp\Exception\RequestException;
 class Handler
 {
     private $client;
+    private $example;
 
     public function __construct() {
         $this->client = new Client([
             'base_uri' => 'https://api.github.com',
+        ]);
+
+        $this->example = json_encode([
+            'repo' => 'owner/repo',
+            'arch' => 'linux,windows,arm64,armhf,darwin',
         ]);
     }
 
     public function handle(string $data): void {
         if ($_SERVER['Http_Content_Type'] === 'application/json') {
             $data = json_decode($data, true);
-            if ($data === null) {
-                echo json_encode([
-                    'repo' => 'owner/repo'
-                ]);
+            if ($data === null || !isset($data['repo'])) {
+                echo $this->example;
                 return;
             } else {
                 $repo = $data['repo'];
+                $arch = $data['arch'];
             }
         } else {
-            $repo = $data;
+            echo "Please use JSON in this format:\n" . $this->example;
+            return;
         }
 
         try {
             $response = $this->client->get('/repos/'.$repo.'/releases/latest');
             $jsonResponse = json_decode($response->getBody()->getContents(), true);
 
-            printf('%s'."\n", $jsonResponse['tag_name']);
+            $data = [
+                'version' => $jsonResponse['tag_name'],
+            ];
 
             foreach ($jsonResponse['assets'] as $asset) {
-                printf('%s - %s'."\n", $asset['name'], $asset['browser_download_url']);
+                if ($this->isArch($arch, $asset['browser_download_url'])) {
+                    $data['name'] = $asset['name'];
+                    $data['url'] = $asset['browser_download_url'];
+                }
             }
+
+            echo json_encode($data);
         } catch (RequestException $e) {
             echo $e->getMessage();
+        }
+    }
+
+    private function isArch($arch, $asset) {
+        switch ($arch) {
+            case 'windows':
+                return strpos($asset, '.exe') !== false;
+            case 'darwin':
+            case 'arm64':
+            case 'armhf':
+                return strpos($asset, $arch) !== false;
+            case 'linux':
+                return !preg_match("/(\.exe|darwin|arm)/", $asset);
         }
     }
 }
